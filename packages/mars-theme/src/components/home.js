a6df45277f2512e52cd8c7647d339a0467f5b966
Andrew Venson <vensoneandrew@gmail.com>
2021-01-21 23:14:58 -0600

    official fixed background finished

--- packages/mars-theme/src/components/home.js
+++ packages/mars-theme/src/components/home.js
@@ -8,10 +8,8 @@ const Home = (props) => {
     useEffect(
         () => {
             window.addEventListener('scroll', handleScroll);
-
             // cleanup
             return () => {
-                console.log("cleanup")
                 window.removeEventListener("scroll", handleScroll)
             }
         }, []
@@ -24,36 +22,27 @@ const Home = (props) => {
                 setScroll(0)
             }else if (window.pageYOffset < 1000){
                 setScroll(1000 - window.pageYOffset)
-                
-                console.log(scroll - window.pageYOffset)
             }
         }
     }
     return (
-        <div>
-            <div style={{zIndex: 999}}>
-                <section style={{height: 650, display: "flex", justifyContent: "center", alignItems: "center"}}>
-                    <h1 style={{color:"white", fontSize: 70}}>KEEP YOUR WORKSPACE SAFE</h1>
-                    <div style={{position: "fixed", top:0, zIndex: -50, height: scroll, overflow: "hidden", bacgroundColor: "rgba(0, 0, 0, 0.2)"}}>
-                        <img src={"https://wallpaperaccess.com/full/2114804.jpg"} style={{width: "100%"}}/>
-                    </div>
-                </section>
+        <div style={{width: "100%"}}>
+            <section style={{display: "flex", justifyContent: "center", alignItems: "center", minHeight: 600, backgroundImage: "url('https://wallpaperaccess.com/full/2114804.jpg')", backgroundAttachment: "fixed", backgroundRepeat: "no-repeat"}}>
+                <h1 style={{color:"white", fontSize: 70}}>KEEP YOUR WORKSPACE SAFE</h1>
+            </section>
 
-                <div style={{backgroundColor: "#1f40a3", width: "100%", height: "633.83px"}}></div>
+            <section style={{ width: "100%", height: "633.83px", display: "flex"}}>
+                <div style={{backgroundColor: "#1f40a3",width: "25%", height: "100%", border: "1px solid black"}} onMouseOver={() => {}}>Test</div>
+                <div style={{backgroundColor: "#1f40a3",width: "25%", height: "100%", border: "1px solid black"}}>Test</div>
+                <div style={{backgroundColor: "#1f40a3",width: "25%", height: "100%", border: "1px solid black"}}>Test</div>
+                <div style={{backgroundColor: "#1f40a3",width: "25%", height: "100%", border: "1px solid black"}}>Test</div>
+            </section>
 
-                <div style={{height: 650, display: "flex", justifyContent: "center", alignItems: "center"}}>
-                    <h1 style={{color:"white", fontSize: 70, overflow: "hidden"}}>BRING YOUR LIGHT INTO THE 21ST CENTURY</h1>
-                    <div style={{position: "fixed", top: 0, zIndex: -100}}>
-                        <img src={"https://images.caricos.com/m/mercedes-benz/2019_mercedes-benz_a-class_sedan/images/1920x1080/2019_mercedes-benz_a-class_sedan_181_1920x1080.jpg"} style={{height: "100%"}}/>
-                    </div>
-                </div>
-                
-                <div style={{backgroundColor: "#1f40a3", width: "100%", height: "633.83px"}}>Hello there</div>
-
-
-
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center", minHeight: 600, backgroundImage: "url('https://images.caricos.com/m/mercedes-benz/2019_mercedes-benz_a-class_sedan/images/1920x1080/2019_mercedes-benz_a-class_sedan_181_1920x1080.jpg')", backgroundAttachment: "fixed", backgroundRepeat: "no-repeat"}}>
+                <h1 style={{color:"white", fontSize: 70, overflow: "hidden"}}>BRING YOUR LIGHT INTO THE 21ST CENTURY</h1>
             </div>
 
+            <div style={{backgroundColor: "#1f40a3", width: "100%", height: "633.83px"}}>Hello there</div>
         </div>
     );
 };
