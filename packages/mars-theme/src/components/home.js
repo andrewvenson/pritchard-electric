d8ace8e1d343027a32f128997d35a62b3b9f2eae
Andrew Venson <vensoneandrew@gmail.com>
2021-01-22 00:13:32 -0600

    added border color transitions to service blocks

--- packages/mars-theme/src/components/home.js
+++ packages/mars-theme/src/components/home.js
@@ -1,46 +1,39 @@
-
 import React, {useEffect, useState} from "react";
+import { styled, connect, Global } from "frontity";
 
 const Home = (props) => {
 
-    const [scroll, setScroll] = useState(1000);
+    const [color, setColor] = useState({
+        one: "#1f40a3",
+        two: "#1f40a3",
+        three: "#1f40a3",
+        four: "#1f40a3"
+    })
 
-    useEffect(
-        () => {
-            window.addEventListener('scroll', handleScroll);
-            // cleanup
-            return () => {
-                window.removeEventListener("scroll", handleScroll)
-            }
-        }, []
-    )
-
-    // image change effect
-    const handleScroll = () => {
-        window.onscroll = () => {
-            if(window.pageYOffset >= 1000){
-                setScroll(0)
-            }else if (window.pageYOffset < 1000){
-                setScroll(1000 - window.pageYOffset)
-            }
-        }
+    const transition = 
+    {
+        width: "25%", height: "100%",
+        transition: "all 1s ease",
+        WebkitTransition: "all 1s ease",
+        MozTransition: "all 1s ease"
     }
+    
     return (
         <div style={{width: "100%"}}>
-            <section style={{display: "flex", justifyContent: "center", alignItems: "center", minHeight: 600, backgroundImage: "url('https://wallpaperaccess.com/full/2114804.jpg')", backgroundAttachment: "fixed", backgroundRepeat: "no-repeat"}}>
+            <section style={{display: "flex", justifyContent: "center", alignItems: "center", minHeight: 600, backgroundImage: "url('https://wallpaperaccess.com/full/2114804.jpg')", backgroundAttachment: "fixed", backgroundRepeat: "no-repeat", backgroundSize: "100% 100%"}}>
                 <h1 style={{color:"white", fontSize: 70}}>KEEP YOUR WORKSPACE SAFE</h1>
             </section>
 
             <section style={{ width: "100%", height: "633.83px", display: "flex"}}>
-                <div style={{backgroundColor: "#1f40a3",width: "25%", height: "100%", border: "1px solid black"}} onMouseOver={() => {}}>Test</div>
-                <div style={{backgroundColor: "#1f40a3",width: "25%", height: "100%", border: "1px solid black"}}>Test</div>
-                <div style={{backgroundColor: "#1f40a3",width: "25%", height: "100%", border: "1px solid black"}}>Test</div>
-                <div style={{backgroundColor: "#1f40a3",width: "25%", height: "100%", border: "1px solid black"}}>Test</div>
+                <div style={{...transition, borderTop: `6px solid ${color.one}`}} onMouseOver={() => {setColor({...color, one: "white"})}} onMouseLeave={() => {setColor({...color, one: "#1f40a3"})}}>Test</div>
+                <div style={{...transition, borderTop: `6px solid ${color.two}`}} onMouseOver={() => {setColor({...color, two: "white"})}} onMouseLeave={() => {setColor({...color, two: "#1f40a3"})}}>Test</div>
+                <div style={{...transition, borderTop: `6px solid ${color.three}`}} onMouseOver={() => {setColor({...color, three: "white"})}} onMouseLeave={() => {setColor({...color, three: "#1f40a3"})}}>Test</div>
+                <div style={{...transition, borderTop: `6px solid ${color.four}`}} onMouseOver={() => {setColor({...color, four: "white"})}} onMouseLeave={() => {setColor({...color, four: "#1f40a3"})}}>Test</div>
             </section>
 
-            <div style={{display: "flex", justifyContent: "center", alignItems: "center", minHeight: 600, backgroundImage: "url('https://images.caricos.com/m/mercedes-benz/2019_mercedes-benz_a-class_sedan/images/1920x1080/2019_mercedes-benz_a-class_sedan_181_1920x1080.jpg')", backgroundAttachment: "fixed", backgroundRepeat: "no-repeat"}}>
-                <h1 style={{color:"white", fontSize: 70, overflow: "hidden"}}>BRING YOUR LIGHT INTO THE 21ST CENTURY</h1>
-            </div>
+            <section style={{display: "flex", justifyContent: "center", alignItems: "center", minHeight: 600, backgroundImage: "url('https://images.caricos.com/m/mercedes-benz/2019_mercedes-benz_a-class_sedan/images/1920x1080/2019_mercedes-benz_a-class_sedan_181_1920x1080.jpg')", backgroundAttachment: "fixed", backgroundRepeat: "no-repeat", backgroundSize: "100% 100%"}}>
+                <h1 style={{color:"white", fontSize: 70}}>BRING YOUR LIGHT INTO THE 21ST CENTURY</h1>
+            </section>
 
             <div style={{backgroundColor: "#1f40a3", width: "100%", height: "633.83px"}}>Hello there</div>
         </div>
